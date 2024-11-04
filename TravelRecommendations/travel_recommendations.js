be3d7591fa026c7769c9aa233a7eb30a97cda5c5
samd474 <148667266+samd474@users.noreply.github.com>
2024-11-04 17:38:50 -0500

    organizing file structure

--- TravelRecommendations/travel_recommendations.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.search-bar button:nth-child(2)').addEventListener('click', () => {
-    document.querySelector('.search-bar input').value = '';
-});
-function clearSearch() {
-    document.getElementById('searchInput').value = '';
-}
